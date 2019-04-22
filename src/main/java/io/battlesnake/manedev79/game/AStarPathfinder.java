52b7598c33a187f74bfb45bff0ce1ca2fec17a22
Matthias Nemeth <manedev79@gmail.com>
2019-04-22 12:28:35 +0200

    Create new A* pathfinder since it is stateful

--- src/main/java/io/battlesnake/manedev79/game/AStarPathfinder.java
+++ src/main/java/io/battlesnake/manedev79/game/AStarPathfinder.java
@@ -13,17 +13,8 @@ import static io.battlesnake.manedev79.game.Path.NO_PATH;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class AStarPathfinder implements Pathfinder {
-    private static final boolean INTERRUPT_IF_RUNNING = true;
-    private static final int PATHFINDER_TIMEOUT_MILLIS = 150;
     private static final Logger LOG = LoggerFactory.getLogger(AStarPathfinder.class);
     private final ExecutorService executorService;
-
-    private PriorityQueue<Field> openSet = new PriorityQueue<>(this::compareFScore);
-    private Set<Field> closedSet = new HashSet<>();
-    private Map<Field, Field> cameFrom = new HashMap<>();
-    private Map<Field, Integer> gScore = new HashMap<>();
-    private Map<Field, Integer> fScore = new HashMap<>();
-    private Board board;
 
     public AStarPathfinder(ExecutorService executorService) {
         this.executorService = executorService;
@@ -31,77 +22,92 @@ public class AStarPathfinder implements Pathfinder {
 
     @Override
     public Path findPath(Board board, Field start, Field destination) {
-        this.board = board;
-        Path path = NO_PATH;
-        FutureTask<Path> pathToFood = new FutureTask<>(() -> Path.of(aStarWay(start, destination)));
-        executorService.submit(pathToFood);
-
-        try {
-            path = pathToFood.get(PATHFINDER_TIMEOUT_MILLIS, MILLISECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            LOG.warn("Calculating path to {} took too long!", destination);
-            pathToFood.cancel(INTERRUPT_IF_RUNNING);
-        }
-
-        return path;
+        return new AStarInternal().findPath(board, start, destination);
     }
 
-    private Collection<Field> aStarWay(Field start, Field destination) {
-        LOG.debug("A* way from {} to {}", start, destination);
-        openSet.add(start);
-        gScore.put(start, 0);
-        fScore.put(start, start.distanceTo(destination));
+    private class AStarInternal implements Pathfinder {
+        private static final boolean INTERRUPT_IF_RUNNING = true;
+        private static final int PATHFINDER_TIMEOUT_MILLIS = 150;
 
-        while (!openSet.isEmpty()) {
-            Field current = openSet.remove();
-            if (current.equals(destination)) {
-                return reconstructPath(cameFrom, current);
+        private PriorityQueue<Field> openSet = new PriorityQueue<>(this::compareFScore);
+        private Set<Field> closedSet = new HashSet<>();
+        private Map<Field, Field> cameFrom = new HashMap<>();
+        private Map<Field, Integer> gScore = new HashMap<>();
+        private Map<Field, Integer> fScore = new HashMap<>();
+        private Board board;
+
+        @Override
+        public Path findPath(Board board, Field start, Field destination) {
+            this.board = board;
+            Path path = NO_PATH;
+            FutureTask<Path> pathToFood = new FutureTask<>(() -> Path.of(aStarWay(start, destination)));
+            executorService.submit(pathToFood);
+
+            try {
+                path = pathToFood.get(PATHFINDER_TIMEOUT_MILLIS, MILLISECONDS);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                LOG.warn("Calculating path to {} took too long!", destination);
+                pathToFood.cancel(INTERRUPT_IF_RUNNING);
             }
-            closedSet.add(current);
-            board.getFreeAdjacentFields(current).stream()
-                 .filter(it -> !closedSet.contains(it))
-                 .forEach(neighbor -> {
-                                             int tentativeGScore = gScoreFor(current) + current.distanceTo(neighbor);
-                                             openSet.add(neighbor);
-                                             if (tentativeGScore < gScoreFor(neighbor)) {
-                                                 cameFrom.put(neighbor, current);
-                                                 gScore.put(neighbor, tentativeGScore);
-                                                 fScore.put(neighbor, tentativeGScore + neighbor.distanceTo(destination));
-                                             }
-                                         }
-                                 );
+
+            return path;
         }
-        LOG.info("There is no path from {} to {}", start, destination);
-        //noinspection unchecked
-        return Collections.EMPTY_LIST;
-    }
 
-    private Integer gScoreFor(Field current) {
-        return gScore.getOrDefault(current, Integer.MAX_VALUE);
-    }
+        private Collection<Field> aStarWay(Field start, Field destination) {
+            LOG.debug("A* way from {} to {}", start, destination);
+            openSet.add(start);
+            gScore.put(start, 0);
+            fScore.put(start, start.distanceTo(destination));
 
-    private Collection<Field> reconstructPath(Map<Field, Field> cameFrom, Field destination) {
-        List<Field> path = new ArrayList<>();
-        path.add(destination);
-        Field current = destination;
-
-        while (cameFrom.containsKey(current)) {
-            current = cameFrom.get(current);
-            path.add(current);
+            while (!openSet.isEmpty()) {
+                Field current = openSet.remove();
+                if (current.equals(destination)) {
+                    return reconstructPath(cameFrom, current);
+                }
+                closedSet.add(current);
+                board.getFreeAdjacentFields(current).stream()
+                     .filter(it -> !closedSet.contains(it))
+                     .forEach(neighbor -> {
+                                 int tentativeGScore = gScoreFor(current) + current.distanceTo(neighbor);
+                                 openSet.add(neighbor);
+                                 if (tentativeGScore < gScoreFor(neighbor)) {
+                                     cameFrom.put(neighbor, current);
+                                     gScore.put(neighbor, tentativeGScore);
+                                     fScore.put(neighbor, tentativeGScore + neighbor.distanceTo(destination));
+                                 }
+                             }
+                     );
+            }
+            LOG.info("There is no path from {} to {}", start, destination);
+            return Collections.EMPTY_LIST;
         }
-        // Glitches:
-        // Invert
-        Collections.reverse(path);
-        // Remove current field
-        path.remove(0);
 
-        LOG.debug("Reconstructed path: {}", path);
-        return path;
+        private Integer gScoreFor(Field current) {
+            return gScore.getOrDefault(current, Integer.MAX_VALUE);
+        }
+
+        private Collection<Field> reconstructPath(Map<Field, Field> cameFrom, Field destination) {
+            List<Field> path = new ArrayList<>();
+            path.add(destination);
+            Field current = destination;
+
+            while (cameFrom.containsKey(current)) {
+                current = cameFrom.get(current);
+                path.add(current);
+            }
+            // Glitches:
+            // Invert
+            Collections.reverse(path);
+            // Remove current field
+            path.remove(0);
+
+            LOG.debug("Reconstructed path: {}", path);
+            return path;
+        }
+
+        private Integer compareFScore(Field field, Field otherField) {
+            return Integer.compare(fScore.getOrDefault(field, Integer.MAX_VALUE),
+                    fScore.getOrDefault(otherField, Integer.MAX_VALUE));
+        }
     }
-
-    private Integer compareFScore(Field field, Field otherField) {
-        return Integer.compare(fScore.getOrDefault(field, Integer.MAX_VALUE),
-                fScore.getOrDefault(otherField, Integer.MAX_VALUE));
-    }
-
 }
